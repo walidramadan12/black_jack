e57b2eea746521d7f6dfeac832e6bafd0d6d0165
walidramadan12 <107707564+walidramadan12@users.noreply.github.com>
2023-08-11 11:58:18 +0300

    got some trouble in :)

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,7 +11,21 @@ const blackjack =[1,2,3,4,5,6,7,8,9,10,11,12,13]
 document.title=("Blackjack")
 
 startGameBtn.addEventListener("click",function(){
-    let card = Math.floor(Math.random(blackjack)*blackjack.length)
+    phraseEl.innerHTML=`Want to start a new game?`
+
+    let card01 = Math.floor(Math.random(blackjack)*blackjack.length)
     let card02 = Math.floor(Math.random(blackjack)*blackjack.length)
-    cardsEl.innerHTML=` Cards: ${blackjack[card]} ${blackjack[card02]} `
+    cardsEl.innerHTML=` Cards: ${blackjack[card01]} ${blackjack[card02]} `
+
+    let sum = blackjack[card01] + blackjack[card02]
+    sumEl.innerHTML = `Sum: ${sum}`
+ 
+    // counter=50
+//    function moneyLeft(){
+//     let given = 200
+//     let counter = 50
+
+//    }
+
+//     nameEl.innerHTML = `Walid:$ ${moneyLeft()}`
 })
